e11f379d1f7d7a41c33d022026359ce7f25fbf32
AEspinosaDev <tonyespinosa96@gmail.com>
2024-05-09 20:50:06 +0200

    Fixed Release Nvidia driver bug

--- include/engine/renderpasses/geometry_pass.h
+++ include/engine/renderpasses/geometry_pass.h
@@ -1,26 +1,30 @@
-// #ifndef DEPTH_PASS_H
-// #define DEPTH_PASS_H
+// #ifndef GEOMETRY_PASS_H
+// #define GEOMETRY_PASS_H
 // #include <engine/core/renderpass.h>
 
 // VULKAN_ENGINE_NAMESPACE_BEGIN
 
-// class DepthPass : public RenderPass
+// class GeometryPass : public RenderPass
 // {
-//     VkSampleCountFlagBits m_samples;
+//     ColorFormatType m_colorFormat;
 //     DepthFormatType m_depthFormat;
+//     VkSampleCountFlagBits m_samples;
 
 // public:
-//     DepthPass(VkExtent2D extent,
-//               uint32_t framebufferCount,
-//               DepthFormatType depthFormat,
-//               VkSampleCountFlagBits samples) : RenderPass(extent, framebufferCount, 1, false),
-//                                                m_depthFormat(depthFormat), m_samples(samples) {}
-
+//     GeometryPass(VkExtent2D extent,
+//                 uint32_t framebufferCount,
+//                 ColorFormatType colorFormat,
+//                 DepthFormatType depthFormat,
+//                 VkSampleCountFlagBits samples) : RenderPass(extent, framebufferCount, 1, true),
+//                                                  m_colorFormat(colorFormat),
+//                                                  m_depthFormat(depthFormat),
+//                                                  m_samples(samples) {}
 //     void init(VkDevice &device);
 
 //     void create_pipelines(VkDevice &device, DescriptorManager &descriptorManager);
 
-//     void render(Frame &frame, uint32_t frameIndex, Scene *const scene, uint32_t framebufferIndex = 0);
+//     void render(Frame &frame, uint32_t frameIndex, Scene *const scene, uint32_t presentImageIndex = 0);
+
 // };
 // VULKAN_ENGINE_NAMESPACE_END
 
--- src/renderpasses/geometry_pass.cpp
+++ src/renderpasses/geometry_pass.cpp
@@ -1,54 +1,104 @@
-// #include <engine/renderpasses/geometry_pass.h>
+// #include <engine/renderpasses/forward_pass.h>
 
 // VULKAN_ENGINE_NAMESPACE_BEGIN
 
-// void DepthPass::create(VkDevice &device, VkCommandPool &commandPool)
+// void GeometryPass::init(VkDevice &device)
 // {
-//     // VkCommandBufferAllocateInfo commandBufferAllocateInfo{};
-//     commandBufferAllocateInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO;
-//     commandBufferAllocateInfo.commandPool = commandPool;
-//     commandBufferAllocateInfo.level = VK_COMMAND_BUFFER_LEVEL_PRIMARY;
-//     commandBufferAllocateInfo.commandBufferCount = m_framebufferCount;
-//     VK_CHECK(vkAllocateCommandBuffers(device, &commandBufferAllocateInfo, m_commandBuffers.data()));
+//     bool multisampled = m_samples > VK_SAMPLE_COUNT_1_BIT;
 
-//     VkAttachmentDescription depthAttachment = init::attachment_description(static_cast<VkFormat>(m_depthFormat),
-//                                                                            VK_IMAGE_LAYOUT_DEPTH_STENCIL_ATTACHMENT_OPTIMAL, VK_IMAGE_LAYOUT_UNDEFINED, VK_SAMPLE_COUNT_1_BIT, true);
-//     // depthAttachment.storeOp = VK_ATTACHMENT_STORE_OP_DONT_CARE;
-//     m_attachments.push_back(Attachment({depthAttachment, VK_IMAGE_USAGE_DEPTH_STENCIL_ATTACHMENT_BIT, VK_IMAGE_ASPECT_DEPTH_BIT}));
+//     std::array<VkAttachmentDescription, 2> attachmentsInfo = {};
 
-//     VkSubpassDependency earlyDepthDep = init::subpass_dependency(VK_PIPELINE_STAGE_EARLY_FRAGMENT_TESTS_BIT | VK_PIPELINE_STAGE_LATE_FRAGMENT_TESTS_BIT,
-//                                                                  VK_PIPELINE_STAGE_EARLY_FRAGMENT_TESTS_BIT | VK_PIPELINE_STAGE_LATE_FRAGMENT_TESTS_BIT,
-//                                                                  VK_ACCESS_DEPTH_STENCIL_ATTACHMENT_WRITE_BIT, VK_ACCESS_DEPTH_STENCIL_ATTACHMENT_READ_BIT, 0, VK_SUBPASS_EXTERNAL);
-//     // earlyDepthDep.dependencyFlags = VK_DEPENDENCY_BY_REGION_BIT;
+//     // Normals attachment
+//     attachmentsInfo[0].format = static_cast<VkFormat>(m_colorFormat);
+//     attachmentsInfo[0].samples = m_samples;
+//     attachmentsInfo[0].loadOp = VK_ATTACHMENT_LOAD_OP_CLEAR;
+//     attachmentsInfo[0].storeOp = VK_ATTACHMENT_STORE_OP_STORE;
+//     attachmentsInfo[0].stencilLoadOp = VK_ATTACHMENT_LOAD_OP_DONT_CARE;
+//     attachmentsInfo[0].stencilStoreOp = VK_ATTACHMENT_STORE_OP_DONT_CARE;
+//     attachmentsInfo[0].initialLayout = VK_IMAGE_LAYOUT_UNDEFINED;
+//     attachmentsInfo[0].finalLayout = VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL;
 
-//     // VkSubpassDependency earlyDepthDep = init::subpass_dependency(VK_PIPELINE_STAGE_FRAGMENT_SHADER_BIT, VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT,
-//     //                                                              VK_ACCESS_SHADER_READ_BIT, VK_ACCESS_COLOR_ATTACHMENT_WRITE_BIT);
-//     // earlyDepthDep.dependencyFlags = VK_DEPENDENCY_BY_REGION_BIT;
+//     m_attachments.push_back(
+//         Attachment(static_cast<VkFormat>(m_colorFormat),
+//                    VK_IMAGE_USAGE_TRANSIENT_ATTACHMENT_BIT | VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT,
+//                    VK_IMAGE_ASPECT_COLOR_BIT,
+//                    VK_IMAGE_VIEW_TYPE_2D,
+//                    m_samples));
 
-//     // VkSubpassDependency lateDepthDep = init::subpass_dependency(VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT, VK_PIPELINE_STAGE_FRAGMENT_SHADER_BIT,
-//     //                                                             VK_ACCESS_COLOR_ATTACHMENT_WRITE_BIT, VK_ACCESS_SHADER_READ_BIT, 0, VK_SUBPASS_EXTERNAL);
-//     // lateDepthDep.dependencyFlags = VK_DEPENDENCY_BY_REGION_BIT;
+//     // VkAttachmentReference colorRef = init::attachment_reference(0, VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL);
 
-//     VkAttachmentReference depthRef = init::attachment_reference(0, VK_IMAGE_LAYOUT_DEPTH_STENCIL_ATTACHMENT_OPTIMAL);
-//     VkSubpassDescription depthSubpass = init::subpass_description(0, VK_NULL_HANDLE, depthRef);
+//     // // Normals attachment
+//     // attachmentsInfo[1].format = static_cast<VkFormat>(m_colorFormat);
+//     // attachmentsInfo[1].samples = VK_SAMPLE_COUNT_1_BIT;
+//     // attachmentsInfo[1].loadOp = VK_ATTACHMENT_LOAD_OP_CLEAR;
+//     // attachmentsInfo[1].storeOp = VK_ATTACHMENT_STORE_OP_STORE;
+//     // attachmentsInfo[1].stencilLoadOp = VK_ATTACHMENT_LOAD_OP_DONT_CARE;
+//     // attachmentsInfo[1].stencilStoreOp = VK_ATTACHMENT_STORE_OP_DONT_CARE;
+//     // attachmentsInfo[1].initialLayout = VK_IMAGE_LAYOUT_UNDEFINED;
+//     // attachmentsInfo[1].finalLayout = VK_IMAGE_LAYOUT_PRESENT_SRC_KHR;
 
-//     std::vector<VkSubpassDescription> subpasses = {depthSubpass};
-//     std::vector<VkSubpassDependency> dependencies = {earlyDepthDep};
+//     // Attachment resolveAttachment(static_cast<VkFormat>(m_colorFormat),
+//     //                              VK_IMAGE_USAGE_TRANSIENT_ATTACHMENT_BIT | VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT,
+//     //                              VK_IMAGE_ASPECT_COLOR_BIT);
+//     // resolveAttachment.isPresentImage = true;
+//     // m_attachments.push_back(resolveAttachment);
 
-//     std::vector<VkAttachmentDescription> attachmentsSubDescriptions;
-//     attachmentsSubDescriptions.reserve(m_attachments.size());
-//     for (Attachment &attachment : m_attachments)
-//     {
-//         attachmentsSubDescriptions.push_back(attachment.description.description);
-//     }
+//     // VkAttachmentReference resolveRef = init::attachment_reference(1, VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL);
 
+//     // Depth attachment
+//     attachmentsInfo[1].format = static_cast<VkFormat>(m_depthFormat);
+//     attachmentsInfo[1].samples = m_samples;
+//     attachmentsInfo[1].loadOp = VK_ATTACHMENT_LOAD_OP_CLEAR;
+//     attachmentsInfo[1].storeOp = VK_ATTACHMENT_STORE_OP_STORE;
+//     attachmentsInfo[1].stencilLoadOp = VK_ATTACHMENT_LOAD_OP_DONT_CARE;
+//     attachmentsInfo[1].stencilStoreOp = VK_ATTACHMENT_STORE_OP_DONT_CARE;
+//     attachmentsInfo[1].initialLayout = VK_IMAGE_LAYOUT_UNDEFINED;
+//     attachmentsInfo[1].finalLayout = VK_IMAGE_LAYOUT_DEPTH_STENCIL_ATTACHMENT_OPTIMAL;
+
+//     m_attachments.push_back(
+//         Attachment(static_cast<VkFormat>(m_depthFormat),
+//                    VK_IMAGE_USAGE_DEPTH_STENCIL_ATTACHMENT_BIT,
+//                    VK_IMAGE_ASPECT_DEPTH_BIT,
+//                    VK_IMAGE_VIEW_TYPE_2D,
+//                    m_samples));
+
+//     VkAttachmentReference depthRef = init::attachment_reference(1, VK_IMAGE_LAYOUT_DEPTH_STENCIL_ATTACHMENT_OPTIMAL);
+
+//     // Subpass
+//     VkSubpassDescription subpass = {};
+//     subpass.pipelineBindPoint = VK_PIPELINE_BIND_POINT_GRAPHICS;
+//     subpass.colorAttachmentCount = 1;
+//     subpass.pColorAttachments = &colorRef;
+//     subpass.pDepthStencilAttachment = &depthRef;
+//     subpass.pResolveAttachments = &resolveRef;
+
+//     // Depdencies
+//     std::array<VkSubpassDependency, 2> dependencies = {};
+
+//     dependencies[0].srcSubpass = VK_SUBPASS_EXTERNAL;
+//     dependencies[0].dstSubpass = 0;
+//     dependencies[0].srcStageMask = VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT;
+//     dependencies[0].dstStageMask = VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT;
+//     dependencies[0].srcAccessMask = 0;
+//     dependencies[0].dstAccessMask = VK_ACCESS_COLOR_ATTACHMENT_WRITE_BIT;
+//     dependencies[0].dependencyFlags = VK_DEPENDENCY_BY_REGION_BIT;
+
+//     dependencies[1].srcSubpass = VK_SUBPASS_EXTERNAL;
+//     dependencies[1].dstSubpass = 0;
+//     dependencies[1].srcStageMask = VK_PIPELINE_STAGE_EARLY_FRAGMENT_TESTS_BIT;
+//     dependencies[1].dstStageMask = VK_PIPELINE_STAGE_EARLY_FRAGMENT_TESTS_BIT;
+//     dependencies[1].srcAccessMask = 0;
+//     dependencies[1].dstAccessMask = VK_ACCESS_DEPTH_STENCIL_ATTACHMENT_WRITE_BIT;
+//     dependencies[1].dependencyFlags = VK_DEPENDENCY_BY_REGION_BIT;
+
+//     // Creation
 //     VkRenderPassCreateInfo renderPassInfo{};
 //     renderPassInfo.sType = VK_STRUCTURE_TYPE_RENDER_PASS_CREATE_INFO;
-//     renderPassInfo.attachmentCount = (uint32_t)attachmentsSubDescriptions.size();
-//     renderPassInfo.pAttachments = attachmentsSubDescriptions.data();
-//     renderPassInfo.subpassCount = (uint32_t)subpasses.size();
-//     renderPassInfo.pSubpasses = subpasses.data();
-//     renderPassInfo.dependencyCount = (uint32_t)dependencies.size();
+//     renderPassInfo.attachmentCount = static_cast<uint32_t>(attachmentsInfo.size());
+//     renderPassInfo.pAttachments = attachmentsInfo.data();
+//     renderPassInfo.subpassCount = 1;
+//     renderPassInfo.pSubpasses = &subpass;
+//     renderPassInfo.dependencyCount = static_cast<uint32_t>(dependencies.size());
 //     renderPassInfo.pDependencies = dependencies.data();
 
 //     if (vkCreateRenderPass(device, &renderPassInfo, nullptr, &m_obj) != VK_SUCCESS)
@@ -58,7 +108,7 @@
 
 //     m_initiatized = true;
 // }
-// void DepthPass::create_graphic_pipelines(VkDevice &device, DescriptorManager &descriptorManager)
+// void GeometryPass::create_pipelines(VkDevice &device, DescriptorManager &descriptorManager)
 // {
 //     PipelineBuilder builder;
 
@@ -70,7 +120,7 @@
 //     builder.vertexInputInfo.pVertexBindingDescriptions = &bindingDescription;
 
 //     // Viewport
-//     builder.viewport = init::viewport(m_extent, 0.0f, 1.0f, 0.0f, 0.0f);
+//     builder.viewport = init::viewport(m_extent);
 //     builder.scissor.offset = {0, 0};
 //     builder.scissor.extent = m_extent;
 
@@ -78,75 +128,110 @@
 
 //     builder.depthStencil = init::depth_stencil_create_info(true, true, VK_COMPARE_OP_LESS);
 
-//     builder.colorBlending.attachmentCount = 0;
+//     builder.multisampling = init::multisampling_state_create_info(m_samples);
 
-//     // DEPTH PASS
-//     ShaderPass *depthPass = new ShaderPass(ENGINE_RESOURCES_PATH "shaders/depth.glsl");
-//     depthPass->settings.descriptorSetLayoutIDs = {{DescriptorLayoutType::GLOBAL_LAYOUT, true},
-//                                                   {DescriptorLayoutType::OBJECT_LAYOUT, true},
-//                                                   {DescriptorLayoutType::TEXTURE_LAYOUT, false}};
-//     depthPass->settings.attributes = {{VertexAttributeType::POSITION, true},
-//                                       {VertexAttributeType::NORMAL, false},
-//                                       {VertexAttributeType::UV, false},
-//                                       {VertexAttributeType::TANGENT, false},
-//                                       {VertexAttributeType::COLOR, false}};
+//     builder.dynamicStates.push_back(VK_DYNAMIC_STATE_DEPTH_TEST_ENABLE);
+//     builder.dynamicStates.push_back(VK_DYNAMIC_STATE_DEPTH_WRITE_ENABLE);
+//     builder.dynamicStates.push_back(VK_DYNAMIC_STATE_CULL_MODE);
 
-//     builder.multisampling = init::multisampling_state_create_info(VK_SAMPLE_COUNT_1_BIT);
+//     // Setup shaderpasses
+//     m_shaderPasses["unlit"] = new ShaderPass(ENGINE_RESOURCES_PATH "shaders/unlit.glsl");
+//     m_shaderPasses["unlit"]->settings.descriptorSetLayoutIDs =
+//         {{DescriptorLayoutType::GLOBAL_LAYOUT, true},
+//          {DescriptorLayoutType::OBJECT_LAYOUT, true},
+//          {DescriptorLayoutType::TEXTURE_LAYOUT, false}};
+//     m_shaderPasses["unlit"]->settings.attributes =
+//         {{VertexAttributeType::POSITION, true},
+//          {VertexAttributeType::NORMAL, false},
+//          {VertexAttributeType::UV, false},
+//          {VertexAttributeType::TANGENT, false},
+//          {VertexAttributeType::COLOR, false}};
 
-//     ShaderPass::build_shader_stages(device, *depthPass);
 
-//     builder.build_pipeline_layout(device, descriptorManager, *depthPass);
-//     builder.build_pipeline(device, m_obj, *depthPass);
+//     for (auto pair : m_shaderPasses)
+//     {
+//         ShaderPass *pass = pair.second;
 
-//     m_shaderPasses["depth"] = depthPass;
+//         ShaderPass::build_shader_stages(device, *pass);
+
+//         builder.build_pipeline_layout(device, descriptorManager, *pass);
+//         builder.build_pipeline(device, m_obj, *pass);
+//     }
 // }
-// VkCommandBuffer DepthPass::render(Frame &frame, uint32_t frameIndex, Scene *const scene, uint32_t framebufferIndex)
+// void GeometryPass::render(Frame &frame, uint32_t frameIndex, Scene *const scene, uint32_t presentImageIndex)
 // {
+//     VkCommandBuffer cmd = frame.commandBuffer;
 
-//     VkCommandBuffer cmd = begin(framebufferIndex);
+//     begin(cmd, presentImageIndex);
 
-//     RenderPass::set_viewport_and_scissor(cmd, m_extent);
+//     // Viewport setup
+//     VkViewport viewport = init::viewport(m_extent);
+//     vkCmdSetViewport(cmd, 0, 1, &viewport);
+//     VkRect2D scissor{};
+//     scissor.offset = {0, 0};
+//     scissor.extent = m_extent;
+//     vkCmdSetScissor(cmd, 0, 1, &scissor);
 
-//     ShaderPass *shaderPass = m_shaderPasses["depth"];
-
-//     vkCmdBindPipeline(cmd, VK_PIPELINE_BIND_POINT_GRAPHICS, shaderPass->pipeline);
+//     // vkCmdSetDepthTestEnable(cmd, true);
+//     // vkCmdSetDepthWriteEnable(cmd, true);
 
 //     if (scene->get_active_camera() && scene->get_active_camera()->is_active())
 //     {
 
-//         int mesh_idx = 0;
+//         unsigned int mesh_idx = 0;
 //         for (Mesh *m : scene->get_meshes())
 //         {
 //             if (m)
 //             {
-//                 if (m->is_active() && m->get_cast_shadows() && m->get_num_geometries() > 0 &&
-//                     m->get_bounding_volume()->is_on_frustrum(scene->get_active_camera()->get_frustrum()))
+//                 if (m->is_active() &&                                                                     // Check if is active
+//                     m->get_num_geometries() > 0 &&                                                        // Check if has geometry
+//                     m->get_bounding_volume()->is_on_frustrum(scene->get_active_camera()->get_frustrum())) // Check if is inside frustrum
 //                 {
+//                     // Offset calculation
 //                     uint32_t objectOffset = frame.objectUniformBuffer.strideSize * mesh_idx;
 //                     uint32_t globalOffset = 0;
 
 //                     for (size_t i = 0; i < m->get_num_geometries(); i++)
 //                     {
+//                         Geometry *g = m->get_geometry(i);
+//                         Material *mat = m->get_material(g->get_material_ID());
+
+//                         // Setup per object render state
+//                         vkCmdSetDepthTestEnable(cmd, mat->get_parameters().depthTest);
+//                         vkCmdSetDepthWriteEnable(cmd, mat->get_parameters().depthWrite);
+//                         vkCmdSetCullMode(cmd, mat->get_parameters().faceCulling ? (VkCullModeFlags)mat->get_parameters().culling : VK_CULL_MODE_NONE);
+
+//                         ShaderPass *shaderPass = m_shaderPasses[mat->get_shaderpass_ID()];
+
+//                         // Bind pipeline
+//                         vkCmdBindPipeline(cmd, VK_PIPELINE_BIND_POINT_GRAPHICS, shaderPass->pipeline);
+
 //                         // GLOBAL LAYOUT BINDING
 //                         uint32_t globalOffsets[] = {globalOffset, globalOffset};
 //                         vkCmdBindDescriptorSets(cmd, VK_PIPELINE_BIND_POINT_GRAPHICS, shaderPass->pipelineLayout, 0, 1, &frame.globalDescriptor.descriptorSet, 2, globalOffsets);
+
 //                         // PER OBJECT LAYOUT BINDING
 //                         uint32_t objectOffsets[] = {objectOffset, objectOffset};
-//                         vkCmdBindDescriptorSets(cmd, VK_PIPELINE_BIND_POINT_GRAPHICS, shaderPass->pipelineLayout, 1, 1,
-//                                                 &frame.objectDescriptor.descriptorSet, 2, objectOffsets);
+//                         vkCmdBindDescriptorSets(cmd, VK_PIPELINE_BIND_POINT_GRAPHICS, shaderPass->pipelineLayout, 1, 1, &frame.objectDescriptor.descriptorSet, 2, objectOffsets);
 
-//                         if (m->get_geometry(i)->is_buffer_loaded())
-//                             Geometry::draw(cmd, m->get_geometry(i));
+//                         // TEXTURE LAYOUT BINDING
+//                         if (shaderPass->settings.descriptorSetLayoutIDs[DescriptorLayoutType::TEXTURE_LAYOUT])
+//                             vkCmdBindDescriptorSets(cmd, VK_PIPELINE_BIND_POINT_GRAPHICS, shaderPass->pipelineLayout, 2, 1, &mat->get_texture_descriptor().descriptorSet, 0, nullptr);
+
+//                         if (g->is_buffer_loaded())
+//                             Geometry::draw(cmd, g);
 //                     }
 //                 }
-//                 mesh_idx++;
 //             }
+//             mesh_idx++;
 //         }
 //     }
 
-//     end(cmd);
+//     //Draw gui contents
+//     if (m_gui)
+//         m_gui->upload_draw_data(cmd);
 
-//     return cmd;
+//     end(cmd);
 // }
 
 // VULKAN_ENGINE_NAMESPACE_END
